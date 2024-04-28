fc9bc401ba452b36fde9d8d509b233ed27f42760
azim <azim.iqbal@schibsted.com>
2024-04-29 00:24:55 +0200

    mhm

--- real-estate/astro.config.mjs
+++ real-estate/astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    imageService: true,
-  }),
+    vite: {
+        ssr: {
+          noExternal: ['accessible-astro-components']
+        }
+      }
 });
