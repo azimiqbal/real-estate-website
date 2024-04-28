c3f911a91a26e7a02f4256e47e766db07fdab533
azim <azim.iqbal@schibsted.com>
2024-04-29 00:47:57 +0200

    Revert "mhm"
    This reverts commit fc9bc401ba452b36fde9d8d509b233ed27f42760.

--- real-estate/astro.config.mjs
+++ real-estate/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        ssr: {
-          noExternal: ['accessible-astro-components']
-        }
-      }
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+  }),
 });
