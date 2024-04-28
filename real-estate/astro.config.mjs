daac3a5a316ea3ead6ebdbd6bce2ed8154098a63
azim <azim.iqbal@schibsted.com>
2024-04-29 00:48:12 +0200

    Revert "feat: comeeon"
    This reverts commit 0da94d19f032be5f0307012f092d57edf5f4579e.

--- real-estate/astro.config.mjs
+++ real-estate/astro.config.mjs
@@ -5,7 +5,5 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    imageService: true,
-  }),
+  adapter: vercel()
 });
